a1c8143c1a9338a3fa2480857325ccded42a2120
Harsha_Pragallapati <harshapragallapati2005@gmail.com>
2025-05-05 15:40:06 +0530

    solved two string problems

---
+++ src/String/ValidAnagrams.java
@@ -0,0 +1,38 @@
+package String;
+
+import java.util.Scanner;
+
+public class ValidAnagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter string 1 (lower case): ");
+        String s1 = sc.nextLine();
+        System.out.println("Enter string 2 (lower case): ");
+        String s2 = sc.nextLine();
+        if (checkAnagram(s1, s2)){
+            System.out.println("Both are anagrams of each other");
+        }
+        else {
+            System.out.println("Strings are not anagrams");
+        }
+    }
+
+    private static boolean checkAnagram(String s1, String s2) {
+        int[] hash = new int[26];
+        int n = s1.length();
+        int m = s2.length();
+        if (n != m){
+            return false;
+        }
+        for (int i = 0; i < n; i++) {
+            hash[s1.charAt(i) - 'a']++;
+            hash[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
