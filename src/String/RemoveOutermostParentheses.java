90d139013b41c8f0d8e74a6388b546725487a3e6
Harsha_Pragallapati <harshapragallapati2005@gmail.com>
2025-05-08 10:47:26 +0530

    solved a string problem to remove outermost parentheses

---
+++ src/String/RemoveOutermostParentheses.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Scanner;
+
+public class RemoveOutermostParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(removeOuterParentheses(s));
+    }
+
+    private static String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int open = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (open > 0) sb.append(c);
+                open++;
+            } else {
+                if (open > 1) sb.append(c);
+                open--;
+            }
+        }
+        return sb.toString();
+    }
+}
