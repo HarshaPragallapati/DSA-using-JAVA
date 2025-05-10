31ca62e875901302abe0a4c45e1192019e802671
Harsha_Pragallapati <harshapragallapati2005@gmail.com>
2025-05-10 16:04:17 +0530

    mastering recursion and backtracking

---
+++ src/Backtracking/Subsets90.java
@@ -0,0 +1,33 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Subsets90 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        subsetsWithDup(0, nums, ans, new ArrayList<>());
+        System.out.println(ans);
+    }
+
+    private static void subsetsWithDup(int ind, int[] nums, List<List<Integer>> ans, List<Integer> list) {
+        ans.add(new ArrayList<>(list));
+        for (int i = ind; i < nums.length; i++) {
+            if (i != ind && nums[i] == nums[i - 1]){
+                continue;
+            }
+            list.add(nums[i]);
+            subsetsWithDup(i + 1, nums, ans, list);
+            list.removeLast();
+        }
+    }
+}
